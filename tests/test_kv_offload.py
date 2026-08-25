41317c7000b3a366ce3578e97ae8ad7518288d38
Timothy Hakobian <thakobian@ibm.com>
2026-08-24 21:51:34 -0400

    copy_tensor_raw test: offload and reload
    Signed-off-by: Timothy Hakobian <thakobian@ibm.com>

---
+++ tests/test_kv_offload.py
@@ -0,0 +1,61 @@
+# Copyright 2026 The Torch-Spyre Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import torch
+
+from torch.testing._internal.common_utils import (
+    TestCase,
+    run_tests,
+)
+
+# TEMPORARY BLOCK FOR RUFF:
+from torch_spyre._C import (  # type: ignore[attr-defined]
+    SharedHostPool,
+    copy_tensor_raw,
+    get_composite_address_handle,
+)
+
+
+class TestSpyre(TestCase):
+    def setUp(self):
+        super().setUp()
+        torch.manual_seed(0xAFFE)
+
+    def test_kv_offload_reload(self):
+        tensor_on_spyre = torch.randn(10, device="spyre", dtype=torch.float16)
+
+        # Composite address handle for the tensor to determine the size of the slot needed in the shared host pool
+        slot_bytes = get_composite_address_handle(tensor_on_spyre).total_size()
+
+        # Create the shared pool with a single slot of the required size
+        pool = SharedHostPool.create_or_attach(
+            "kv_offload_pool", num_slots=1, slot_bytes=slot_bytes
+        )
+
+        # Use the first slot in the pool
+        slot_id = 0
+
+        # D2H: Move tensor from spyre to host memory pool
+        copy_tensor_raw(tensor_on_spyre, pool, slot_id, to_device=False)
+
+        # H2D: Move tensor back from host memory pool to spyre
+        tensor_reloaded = torch.empty_like(tensor_on_spyre, device="spyre")
+        copy_tensor_raw(tensor_reloaded, pool, slot_id, to_device=True)
+
+        self.assertEqual(tensor_on_spyre, tensor_reloaded)
+
+
+if __name__ == "__main__":
+    run_tests()
